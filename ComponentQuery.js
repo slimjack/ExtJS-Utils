61441aad31febc02506150f62fefb8389e15740c
Eugene Khudoy <eugene.khudoy@gmail.com>
2015-02-24 22:25:31 +0200

    Component query event relaying

--- ComponentQuery.js
+++ ComponentQuery.js
@@ -1,6 +1,7 @@
 ﻿Ext.define('Ext.ux.util.ComponentQuery', {
     alternateClassName: 'ComponentQuery',
-    defaultMethods: ['on', 'un', 'mon', 'mun', 'disable', 'enable', 'setReadOnly'],
+    _defaultMethods: ['disable', 'enable', 'setReadOnly'],
+    _defaultEvents: [],
     mixins: ['Ext.util.Observable'],
     constructor: function (config, query, excludeQuery) {
         var me = this;
@@ -11,8 +12,8 @@
                 excludeQuery: excludeQuery
             };
         }
-        this.mixins.observable.constructor.call(this, config);
         Ext.apply(this, config);
+        this.mixins.observable.constructor.call(this, config);
         if (!me.query) {
             Ext.Error.raise('"query" is not specified');
         }
@@ -22,17 +23,30 @@
         if (!(me.view instanceof Ext.AbstractComponent)) {
             Ext.Error.raise('"view" is not an instance of Ext.AbstractComponent');
         }
-        me.methods = Ext.Array.union(me.defaultMethods, Ext.Array.from(me.methods));
-        me.createProxyMethod(me.methods);
+
+        me._events = Ext.Array.union(me._defaultEvents, Ext.Array.from(me.events));
+        delete me.events;
+        me.relayComponentsEvents();
+
+        var methods = Ext.Array.union(me._defaultMethods, Ext.Array.from(me.methods));
+        delete me.methods;
+        me.createProxyMethod(methods);
+
         if (me.view instanceof Ext.container.Container) {
             me.view.on('add', me.onAddComponent, me);
             me.view.on('remove', me.onAddComponent, me);
         }
     },
 
+    relayComponentsEvents: function () {
+        var me = this;
+        me.each(function(component) {
+            me.relayEvents(component, me._events);
+        });
+    },
+
     createProxyMethod: function (methods) {
         var me = this;
-        methods = Ext.Array.from(methods);
         Ext.Array.forEach(methods, function (method) {
             if (!me[method]) {
                 me[method] = Ext.bind(me.invoke, me, [method], 0);
@@ -83,7 +97,10 @@
         var newComponents = me.select();
         var addedComponents = Ext.Array.difference(newComponents, oldComponents);
         if (addedComponents.length) {
-            me.fireEvent('add', addedComponents);
+            Ext.Array.each(addedComponents, function (component) {
+                me.relayEvents(component, me._events);
+            });
+            me.fireEvent('queryadd', addedComponents);
         }
     },
 
@@ -94,7 +111,7 @@
         var newComponents = me.select();
         var removedComponents = Ext.Array.difference(oldComponents, newComponents);
         if (removedComponents.length) {
-            me.fireEvent('remove', removedComponents);
+            me.fireEvent('queryremove', removedComponents);
         }
     }
 });
