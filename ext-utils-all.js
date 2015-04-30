a0b1bd7930242028cd2f56dc32bf748cdabcc45e
Eugene Khudoy <eugene.khudoy@gmail.com>
2015-05-01 00:32:05 +0300

    Added 'abstractFn' to Ext namespace

--- ext-utils-all.js
+++ ext-utils-all.js
@@ -1,4 +1,19 @@
-﻿///#source 1 1 /src/Lookup.js
+﻿///#source 1 1 /src/ExtExtentions.js
+Ext.abstractFn = function(msgTpl) {
+    var fn = function() {
+        var thisMethodName = fn.$name;
+        var callerMethodName = fn.caller.$name;
+        if ((thisMethodName !== callerMethodName) && (callerMethodName !== 'callParent')) {
+            var className = fn.caller.$owner.$className;
+            var fullMethodName = className ? className + '.' + thisMethodName : thisMethodName;
+            var message = Ext.String.format(msgTpl || "Method '{0}' is abstract and can't be called.", fullMethodName);
+            Ext.Error.raise(message);
+        }
+    };
+    return fn;
+};
+
+///#source 1 1 /src/Lookup.js
 //https://github.com/slimjack/ExtJs-Utils
 
 Ext.define('Ext.ux.util.Lookup', {
---
+++ src/ExtExtentions.js
@@ -0,0 +1,13 @@
+﻿Ext.abstractFn = function(msgTpl) {
+    var fn = function() {
+        var thisMethodName = fn.$name;
+        var callerMethodName = fn.caller.$name;
+        if ((thisMethodName !== callerMethodName) && (callerMethodName !== 'callParent')) {
+            var className = fn.caller.$owner.$className;
+            var fullMethodName = className ? className + '.' + thisMethodName : thisMethodName;
+            var message = Ext.String.format(msgTpl || "Method '{0}' is abstract and can't be called.", fullMethodName);
+            Ext.Error.raise(message);
+        }
+    };
+    return fn;
+};
