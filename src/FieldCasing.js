8bb84d784b17da50684af307eed3a1ec17a69515
Eugene Khudoy <eugene.khudoy@gmail.com>
2017-03-19 14:35:31 +0200

    FieldCasing plugin added

---
+++ src/FieldCasing.js
@@ -0,0 +1,47 @@
+﻿Ext.define('Ext.ux.util.TextCasings', {
+    alternateClassName: 'TextCasings',
+    statics: {
+        upper: 'upper',
+        lower: 'lower'
+    }
+});
+
+Ext.define('Ext.ux.util.FieldCasing', {
+    alias: 'plugin.fieldcasing',
+    extend: 'Ext.AbstractPlugin',
+
+    toUpperCase: true,
+
+    init: function (field) {
+        var me = this;
+        me.addFieldCasing(field);
+    },
+
+    addFieldCasing: function (field) {
+        var me = this;
+        var originalStyle = field.fieldStyle ? field.fieldStyle + ';' : '';
+        Ext.override(field, {
+            setCasing: function (casing) {
+                this.casing = casing;
+                if (this.casing === TextCasings.upper) {
+                    field.setFieldStyle(originalStyle + "text-transform:uppercase");
+                } else if (this.casing === TextCasings.lower) {
+                    field.setFieldStyle(originalStyle + "text-transform:lowercase");
+                } else {
+                    field.setFieldStyle(originalStyle);
+                }
+            },
+
+            getRawValue: function () {
+                this.rawValue = this.callParent(arguments);
+                if (this.casing === TextCasings.upper) {
+                    this.rawValue = this.rawValue.toUpperCase();
+                } else if (this.casing === TextCasings.lower) {
+                    this.rawValue = this.rawValue.toLowerCase();
+                }
+                return this.rawValue;
+            }
+        });
+        field.setCasing(field.casing);
+    }
+});
